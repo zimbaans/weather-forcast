4d3b13c58d71fcdfe1bc0804a21580dbd340ca51
zimbaan <kapiladhithya@gmail.com>
2023-09-05 11:15:22 +0530

    kapil

--- src/App.jsx
+++ src/App.jsx
@@ -40,6 +40,38 @@ class App extends React.Component {
       return true;
     } else return false;
   };
+
+  getDayIndices = data => {
+    let dayIndices = [];
+    dayIndices.push(0);
+
+    let index = 0;
+    let tmp = data.list[index].dt_txt.slice(8, 10);
+
+    for (let i = 0; i < 4; i++) {
+      while (
+        tmp === data.list[index].dt_txt.slice(8, 10) ||
+        data.list[index].dt_txt.slice(11, 13) !== '15'
+      ) {
+        index++;
+      }
+      dayIndices.push(index);
+      tmp = data.list[index].dt_txt.slice(8, 10);
+    }
+    return dayIndices;
+  };
+
+  render() {
+    const WeatherBoxes = () => {
+      const weatherBoxes = this.state.days.slice(1).map(day => (
+        <li>
+          <WeatherBox {...day} />
+        </li>
+      ));
+
+      return <ul className='weather-box-list'>{weatherBoxes}</ul>;
+    };
+ }
 }
 
 
