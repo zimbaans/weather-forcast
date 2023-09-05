b68f2c91eb7f91db7867b176e0337b04c62f1315
Vikash187 <vikashstanite1807@gmail.com>
2023-09-05 11:40:27 +0530

    Added a LifeCycleComponent

--- src/components/WeatherBox.jsx
+++ src/components/WeatherBox.jsx
@@ -6,4 +6,18 @@ export default class WeatherBox extends React.Component {
     icon: null, 
   };
 
+    componentDidMount() {
+    
+    import(`../images/${this.props.icon}.svg`)
+      .then((module) => {
+        
+        this.setState({ icon: module.default });
+      })
+      .catch((error) => {
+        
+        console.error('Error loading icon:', error);
+      });
+  }
+
+
 }
